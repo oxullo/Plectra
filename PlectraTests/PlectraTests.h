007ff79cc134fff4d8529cfd11181f9b5bde2a23
OXullo Intersecans <x@brainrapers.org>
2016-05-20 19:04:00 +0200

    Auto-update to XCode 7.3

--- PlectraTests/PlectraTests.h
+++ PlectraTests/PlectraTests.h
@@ -6,8 +6,8 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
-@interface PlectraTests : SenTestCase
+@interface PlectraTests : XCTestCase
 
 @end
