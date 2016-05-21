56c910afd514efe7b238919cc1bb371cdeba11ec
OXullo Intersecans <x@brainrapers.org>
2016-05-22 00:33:18 +0200

    Added proper license info

--- Plectra/AppDelegate.m
+++ Plectra/AppDelegate.m
@@ -1,10 +1,23 @@
-//
-//  AppDelegate.m
-//  Plectra
-//
-//  Created by OXullo Intersecans on 18/03/12.
-//  Copyright (c) 2012-2016 bRAiNRAPERS.org. All rights reserved.
-//
+/*
+ Plectra - no-frills audio player
+ 
+ Copyright (C) 2012-2016  OXullo Intersecans <x@brainrapers.org>
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ */
+
 
 #import <AVFoundation/AVPlayerItem.h>
 #import <AVFoundation/AVAsset.h>
