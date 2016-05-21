36a71912c78c950543f9053203cf84be386aebe9
OXullo Intersecans <x@brainrapers.org>
2016-05-21 19:52:40 +0200

    Various copyright cram fixes

--- Plectra/AppDelegate.h
+++ Plectra/AppDelegate.h
@@ -3,7 +3,7 @@
 //  Plectra
 //
 //  Created by OXullo Intersecans on 18/03/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012-2016 bRAiNRAPERS.org. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
